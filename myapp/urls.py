456284d965b4c2317b11848e1f3efddffb837bbe
Lukasz Wyrzykowski <wyrzykow@gmail.com>
2019-12-07 17:16:29 -0800

    fixed bhlist template to add grouping

--- myapp/urls.py
+++ myapp/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     path('datatools/', include('datatools.urls')),
     path('about/', TemplateView.as_view(template_name='tom_common/about.html'), name='about'),
     path('bhlist/', BlackHoleListView.as_view(template_name='tom_common/bhlist.html'), name='bhlist'),
-    path('targets/', BlackHoleListView.as_view(template_name='tom_common/bhlist.html'), name='targets'),
-    path('targets/list', BlackHoleListView.as_view(template_name='tom_common/bhlist.html'), name='targets'),
+    path('targets/', BlackHoleListView.as_view(template_name='tom_common/bhlist.html'), name='bhlist'),
+    path('targets/list', BlackHoleListView.as_view(template_name='tom_common/bhlist.html'), name='bhlist'),
     # The static helper below only works in development see
     # https://docs.djangoproject.com/en/2.1/howto/static-files/#serving-files-uploaded-by-a-user-during-development
  ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
