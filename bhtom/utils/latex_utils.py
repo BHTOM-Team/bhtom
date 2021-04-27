eec9abb643788ac22acbcb76801484dd53c1cf63
Maja Jabłońska <majajjablonska@gmail.com>
2021-04-28 01:05:52 +0200

    Change tabular to tabularx in latex table generator

--- bhtom/utils/latex_utils.py
+++ bhtom/utils/latex_utils.py
@@ -1,12 +1,12 @@
 from typing import Any, List
 from pylatex import Document
-from pylatex.table import Tabular
+from pylatex.table import Tabularx
 
 
 def data_to_latex_table(data: List[List[Any]], columns: List[str], filename: str) -> str:
     doc: Document = Document(filename)
 
-    with doc.create(Tabular("l l l")) as table:
+    with doc.create(Tabularx("lX lX l", width=3)) as table:
         table.add_hline()
         table.add_row([col.replace('_', ' ') for col in columns])
         table.add_hline()
