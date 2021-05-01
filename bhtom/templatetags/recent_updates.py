d5f56e4864360ef9a2eb129a4351d5ae01756b8c
Maja Jabłońska <majajjablonska@gmail.com>
2021-05-01 14:45:08 +0200

    Check if target exists

--- bhtom/templatetags/recent_updates.py
+++ bhtom/templatetags/recent_updates.py
@@ -13,6 +13,7 @@ def recent_comments_with_targets(limit=5):
     with_targets = []
     for comment in Comment.objects.all().order_by('-submit_date')[:limit]:
         target: Target = Target.objects.get(pk=comment.object_pk)
-        with_targets.append({'target': target,
-                             'comment': comment})
+        if target:
+            with_targets.append({'target': target,
+                                 'comment': comment})
     return {'comment_list': with_targets}
