40a5c15ba08ebecb86b741942f5eb285248aa68b
Maja Jabłońska <majajjablonska@gmail.com>
2021-05-17 12:34:28 +0200

    Merge publications, TNS corrections and observer's name and facility name form

--- bhtom/static/bhtom/data_upload.js
+++ bhtom/static/bhtom/data_upload.js
@@ -12,14 +12,18 @@ function dataProductSelect() {
     var dryRun = document.getElementById("id_dryRun");
     var observatory = document.getElementById("id_observatory");
     var filter = document.getElementById("id_filter");
+    var observer = document.getElementById("id_observer");
+    var facility = document.getElementById("id_facility");
 
-    if (data_type_0 == true){
+    if (data_type_0 === true){
 
         mjd.setAttribute("required", true);
         ExpTime.setAttribute("required", true);
         matchDist.setAttribute("required", true);
         observatory.setAttribute("required", true);
         filter.setAttribute("required", true);
+        observer.removeAttribute("required");
+        facility.removeAttribute("required");
 
         mjd.parentElement.style.display = "block";
         ExpTime.parentElement.style.display = "block";
@@ -27,16 +31,20 @@ function dataProductSelect() {
         dryRun.parentElement.style.display = "block";
         observatory.parentElement.style.display = "block";
         filter.parentElement.style.display = "block";
+        observer.parentElement.style.display = "none";
+        facility.parentElement.style.display = "none";
 
     }
 
-    if (data_type_1 == true){
+    if (data_type_1 === true){
 
         mjd.removeAttribute("required");
         ExpTime.removeAttribute("required");
         matchDist.setAttribute("required", true);
         observatory.setAttribute("required", true);
         filter.setAttribute("required", true);
+        observer.removeAttribute("required");
+        facility.removeAttribute("required");
 
         mjd.parentElement.style.display = "none";
         ExpTime.parentElement.style.display = "none";
@@ -44,16 +52,20 @@ function dataProductSelect() {
         dryRun.parentElement.style.display = "block";
         observatory.parentElement.style.display = "block";
         filter.parentElement.style.display = "block";
+        observer.parentElement.style.display = "none";
+        facility.parentElement.style.display = "none";
 
     }
 
-    if (data_type_2 == true || data_type_3 == true || data_type_4 == true){
+    if (data_type_2 === true || data_type_3 === true) {
 
         mjd.removeAttribute("required");
         ExpTime.removeAttribute("required");
         matchDist.removeAttribute("required");
         observatory.removeAttribute("required");
         filter.removeAttribute("required");
+        observer.setAttribute("required", true);
+        facility.setAttribute("required", true);
 
         mjd.parentElement.style.display = "none";
         ExpTime.parentElement.style.display = "none";
@@ -61,6 +73,29 @@ function dataProductSelect() {
         dryRun.parentElement.style.display = "none";
         observatory.parentElement.style.display = "none";
         filter.parentElement.style.display = "none";
+        observer.parentElement.style.display = "block";
+        facility.parentElement.style.display = "block";
+
+    }
+
+    if (data_type_4 === true) {
+
+        mjd.removeAttribute("required");
+        ExpTime.removeAttribute("required");
+        matchDist.removeAttribute("required");
+        observatory.removeAttribute("required");
+        filter.removeAttribute("required");
+        observer.removeAttribute("required");
+        facility.removeAttribute("required");
+
+        mjd.parentElement.style.display = "none";
+        ExpTime.parentElement.style.display = "none";
+        matchDist.parentElement.style.display = "none";
+        dryRun.parentElement.style.display = "none";
+        observatory.parentElement.style.display = "none";
+        filter.parentElement.style.display = "none";
+        observer.parentElement.style.display = "none";
+        facility.parentElement.style.display = "none";
 
     }
 
