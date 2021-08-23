0a9cd492d902962bdcdc2c6033c0afcdbc495ca8
Maja Jabłońska <majajjablonska@gmail.com>
2021-08-23 16:48:55 +0200

    Add cron job updating all target lightcurves every 2 hours

--- settings/settings.py
+++ settings/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_cron',
     'django_extensions',
     'guardian',
     'tom_common',
