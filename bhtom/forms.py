2611877e61bf8201bbd5dd84d903f3dd4d3c59e4
Maja Jabłońska <majajjablonska@gmail.com>
2021-11-02 17:53:49 +0100

    Observatory label is "observatory_name (prefix)"

--- bhtom/forms.py
+++ bhtom/forms.py
@@ -20,20 +20,23 @@ from tom_targets.models import (
 from captcha.fields import ReCaptchaField
 
 import logging
+
 logger = logging.getLogger(__name__)
+
 
 class ObservatoryChoiceField(forms.ModelChoiceField):
 
     def label_from_instance(self, obj):
-        if obj.cpcsOnly == True:
-            return '{obsName} (Only Instrumental photometry file)'.format(obsName=obj.obsName)
+        if obj.cpcsOnly:
+            return '{obsName} ({prefix}) (Only Instrumental photometry file)'.format(obsName=obj.obsName,
+                                                                                     prefix=obj.prefix)
         else:
-            return '{obsName}'.format(obsName=obj.obsName)
+            return '{obsName} ({prefix})'.format(obsName=obj.obsName, prefix=obj.prefix)
+
 
 class FilterChoiceField(forms.ModelChoiceField):
 
     def label_from_instance(self, obj):
-
         name = obj.name
         filters = obj.filters
         hash = {}
@@ -42,8 +45,8 @@ class FilterChoiceField(forms.ModelChoiceField):
 
         return hash
 
-class DataProductUploadForm(forms.Form):
 
+class DataProductUploadForm(forms.Form):
     MATCHING_RADIUS = [
         ('0', 'Default for the Observatory'),
         ('1', '1 arcsec'),
@@ -67,12 +70,12 @@ class DataProductUploadForm(forms.Form):
         widget=forms.ClearableFileInput(
             attrs={'multiple': True}
         )
-     )
+    )
 
     data_product_type = forms.ChoiceField(
         choices=[v for k, v in settings.DATA_PRODUCT_TYPES.items()],
         initial='photometry_cpcs',
-        widget=forms.RadioSelect(attrs={'onclick' : "dataProductSelect();"}),
+        widget=forms.RadioSelect(attrs={'onclick': "dataProductSelect();"}),
         required=True
     )
 
@@ -155,8 +158,8 @@ class DataProductUploadForm(forms.Form):
 
         self.fields['observer'].initial = f'{user.first_name} {user.last_name}'
 
-class ObservatoryCreationForm(forms.ModelForm):
 
+class ObservatoryCreationForm(forms.ModelForm):
     cpcsOnly = forms.BooleanField(
         label='Only instrumental photometry file',
         required=False
@@ -166,8 +169,8 @@ class ObservatoryCreationForm(forms.ModelForm):
         model = Observatory
         fields = ('obsName', 'lon', 'lat', 'matchDist', 'cpcsOnly', 'fits', 'obsInfo', 'comment')
 
-class ObservatoryUpdateForm(forms.ModelForm):
 
+class ObservatoryUpdateForm(forms.ModelForm):
     cpcsOnly = forms.BooleanField(
         label='Only instrumental photometry file',
         required=False
@@ -177,14 +180,14 @@ class ObservatoryUpdateForm(forms.ModelForm):
         model = Observatory
         fields = ('obsName', 'lon', 'lat', 'matchDist', 'prefix', 'cpcsOnly', 'fits', 'obsInfo', 'comment')
 
-class InstrumentUpdateForm(forms.ModelForm):
 
+class InstrumentUpdateForm(forms.ModelForm):
     class Meta:
         model = Instrument
         fields = ('comment',)
 
-class InstrumentCreationForm(forms.Form):
 
+class InstrumentCreationForm(forms.Form):
     observatory = forms.ChoiceField()
 
     comment = forms.CharField(
@@ -194,7 +197,6 @@ class InstrumentCreationForm(forms.Form):
     )
 
     def __init__(self, *args, **kwargs):
-
         user = kwargs.pop('user')
         super(InstrumentCreationForm, self).__init__(*args, **kwargs)
 
@@ -210,6 +212,7 @@ class InstrumentCreationForm(forms.Form):
             required=True
         )
 
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     groups = forms.ModelMultipleChoiceField(Group.objects.all().exclude(name='Public'),
@@ -223,7 +226,7 @@ class CustomUserCreationForm(UserCreationForm):
         required=False
     )
 
-    #captcha = ReCaptchaField()
+    # captcha = ReCaptchaField()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -262,6 +265,7 @@ class CustomUserCreationForm(UserCreationForm):
 
         return user
 
+
 def extra_field_to_form_field(field_type):
     if field_type == 'number':
         return forms.FloatField(required=False)
@@ -275,6 +279,7 @@ def extra_field_to_form_field(field_type):
         raise ValueError(
             'Invalid field type {}. Field type must be one of: number, boolean, datetime string'.format(field_type)
         )
+
 
 class CoordinateField(forms.CharField):
     def __init__(self, *args, **kwargs):
@@ -320,9 +325,9 @@ class TargetForm(forms.ModelForm):
             for field in settings.EXTRA_FIELDS:
                 if self.cleaned_data.get(field['name']) is not None:
                     TargetExtra.objects.update_or_create(
-                            target=instance,
-                            key=field['name'],
-                            defaults={'value': self.cleaned_data[field['name']]}
+                        target=instance,
+                        key=field['name'],
+                        defaults={'value': self.cleaned_data[field['name']]}
                     )
 
         return instance
@@ -351,6 +356,7 @@ class SiderealTargetCreateForm(TargetForm):
 
     class Meta(TargetForm.Meta):
         fields = SIDEREAL_FIELDS
+
 
 class NonSiderealTargetCreateForm(TargetForm):
     def __init__(self, *args, **kwargs):
@@ -381,6 +387,7 @@ class NonSiderealTargetCreateForm(TargetForm):
 
     class Meta(TargetForm.Meta):
         fields = NON_SIDEREAL_FIELDS
+
 
 class TargetVisibilityForm(forms.Form):
     start_time = forms.DateTimeField(required=True, label='Start Time', widget=forms.TextInput(attrs={'type': 'date'}))
