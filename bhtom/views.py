8c121fde8661df23c82baef354db9c9abc2557f4
Artur Krawczyk <arturkrawczyk19@gmail.com>
2022-03-10 13:34:54 +0100

    revoke error: file exits

--- bhtom/views.py
+++ bhtom/views.py
@@ -669,16 +669,16 @@ class fits_upload(viewsets.ModelViewSet):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         successful_uploads = []
-        BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        #BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
         for f in data_product_files:
 
             f.name = "{}_{}".format(user.id, f.name)
 
-            if os.path.exists('{0}/data/{1}/none/{2}'.format(BASE, target, f.name)):
-                messages.error(self.request, read_secret('FILE_EXIST'))
-                logger.error('File exits: %s %s' % (str(f.name), str(target)))
-                return Response(status=status.HTTP_201_CREATED)
+            #if os.path.exists('{0}/data/{1}/none/{2}'.format(BASE, target, f.name)):
+             #   messages.error(self.request, read_secret('FILE_EXIST'))
+             #   logger.error('File exits: %s %s' % (str(f.name), str(target)))
+             #   return Response(status=status.HTTP_201_CREATED)
 
             dp = DataProduct(
                 target=target_id,
@@ -844,15 +844,15 @@ class DataProductUploadView(FormView):
             return redirect(form.cleaned_data.get('referrer', '/'))
 
         successful_uploads = []
-        BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        #BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         for f in data_product_files:
 
             f.name = "{}_{}".format(user.id, f.name)
 
-            if os.path.exists('{0}/data/{1}/none/{2}'.format(BASE, target, f.name)):
-                messages.error(self.request, read_secret('FILE_EXIST'))
-                logger.error('File exits: %s %s' % (str(f.name), str(target)))
-                return redirect(form.cleaned_data.get('referrer', '/'))
+            #if os.path.exists('{0}/data/{1}/none/{2}'.format(BASE, target, f.name)):
+             #   messages.error(self.request, read_secret('FILE_EXIST'))
+             #   logger.error('File exits: %s %s' % (str(f.name), str(target)))
+             #   return redirect(form.cleaned_data.get('referrer', '/'))
 
             dp = DataProduct(
                 target=target,
